5b952440116c1f76e4efe89df7eb3250e047884e
BogdanNuExista <bogdancraiu15@gmail.com>
2025-06-01 12:11:04 +0300

    finished default implementation

---
+++ assignment5/default/Bookdata.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Bookdata {
+    public List<Persondata> author = new ArrayList<>();
+    public String title;
+    public float price;
+    public String description;
+    public String id;
+}
